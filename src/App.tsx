c476f5f6cd37ec31c3834f9c798789d9ba825593
Denny Biasiolli <denny.biasiolli@gmail.com>
2019-05-21 16:27:04 +0200

    removing unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 
-import { Counter } from './components/Counter';
 import { Game } from './components/Game';
 
 
 export function App() {
   return <div>
     <Game />
-    <Counter />
   </div>;
 }
