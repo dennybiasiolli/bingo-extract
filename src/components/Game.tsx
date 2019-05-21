a444b1b34e0e59c1f5edf7522db0b2f202226e5d
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2019-05-21 16:27:04 +0200

    removing unused code

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -4,70 +4,28 @@ import { Dispatch, bindActionCreators } from 'redux';
 import { Button, CardContent } from '@material-ui/core';
 
 import { BingoBoard } from './BingoBoard';
-import { Board } from './Board';
 import { RootState } from '../store';
 import { extract } from '../store/reducers/bingo-actions';
-import { makeMove, jumpTo } from '../store/reducers/tictactoe-actions';
 
 
 const mapStateToProps = (state: RootState) => ({
-  current: state.tictactoe.current,
-  history: state.tictactoe.history,
-  stepNumber: state.tictactoe.stepNumber,
-  winner: state.tictactoe.winner,
-  xIsNext: state.tictactoe.xIsNext,
   numbers: state.bingo.numbers,
 });
 
 const mapDispatchToProps = (dispatch: Dispatch) => bindActionCreators({
-  makeMove: makeMove,
-  jumpTo: jumpTo,
   extract: extract,
 }, dispatch);
 
 
 export interface GameProps extends ReturnType<typeof mapStateToProps>, ReturnType<typeof mapDispatchToProps> { };
 
-function GameBase({ current, history, winner, xIsNext, numbers, makeMove, jumpTo, extract }: GameProps) {
-  const moves = history.map((_step, move) => {
-    const desc = move ?
-      'Go to move #' + move :
-      'Go to game start';
-    return (
-      <li key={move}>
-        <Button variant="contained" onClick={() => jumpTo(move)}>{desc}</Button>
-      </li>
-    );
-  });
-
-  let status;
-  if (winner) {
-    status = 'Winner: ' + winner;
-  } else {
-    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
-  }
-
+function GameBase({ numbers, extract }: GameProps) {
   return (
     <CardContent>
-      <div className="game">
-        <div className="game-board">
-          <Board
-            squares={current.squares}
-            onClick={makeMove}
-          />
-        </div>
-        <div className="game-info">
-          <div>{status}</div>
-          <ol>{moves}</ol>
-        </div>
-      </div>
-      <br />
-      <div>
-        <Button variant="contained" onClick={() => extract()}>Extract</Button>
-        <BingoBoard
-          numbers={numbers}
-        />
-      </div>
+      <Button variant="contained" onClick={() => extract()}>Extract</Button>
+      <BingoBoard
+        numbers={numbers}
+      />
     </CardContent>
   );
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,19 +1,13 @@
 import { createStore, combineReducers } from 'redux';
 
-import { CounterReducer } from './reducers/counter-reducer';
-import { CounterActionTypes } from './reducers/counter-types';
-import { TictactoeReducer } from './reducers/tictactoe-reducer';
-import { TictactoeActionTypes } from './reducers/tictactoe-types';
 import { BingoReducer } from './reducers/bingo-reducer';
 import { BingoActionTypes } from './reducers/bingo-types';
 
 
 const rootReducer = combineReducers({
   bingo: BingoReducer,
-  counter: CounterReducer,
-  tictactoe: TictactoeReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
-export type RootAction = BingoActionTypes | CounterActionTypes | TictactoeActionTypes;
+export type RootAction = BingoActionTypes;
 export default () => createStore<RootState, RootAction, {}, {}>(rootReducer);
